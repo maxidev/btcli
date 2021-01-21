a563996491356439d35104b6c9869387faf90ac0
Maxi Dev <maxi.canellas@gmail.com>
2021-01-21 16:09:48 -0300

    [TEST] just testing

--- index.js
+++ index.js
@@ -8,6 +8,33 @@ const axios = require('axios');
 const log = console.log;
 
 
+// COMMON OPCODE list in hex
+const OP_0           = '00';
+const OP_1           = '51';
+const OP_2           = '52';
+const OP_3           = '53';
+const OP_4           = '54';
+const OP_5           = '55';
+const OP_6           = '56';
+const OP_7           = '57';
+const OP_8           = '58';
+const OP_9           = '59';
+const OP_10          = '5a';
+const OP_11          = '5b';
+const OP_12          = '5c';
+const OP_13          = '5d';
+const OP_14          = '5e';
+const OP_15          = '5f';
+const OP_16          = '60';
+const OP_RETURN      = '6a';
+const OP_DUP         = '76';
+const OP_EQUAL       = '87';
+const OP_EQUALVERIFY = '88';
+const OP_CHECKSIG    = 'ac';
+const OP_CHECKMULTISIG = 'ae';
+const OP_HASH160     = 'a9';
+
+
 program
   .description('BTCli - A simple command line Bitcoin explorer')
   .version('0.0.1', '-v, --vers', 'output the current version')
@@ -36,43 +63,55 @@ async function main () {
     await client.connect();
 
     console.log("-------------------------");
-    //console.log(options);
 
     let addr = options.address;
 
+
     let script = bitcoin.address.toOutputScript(addr)
+    
+    //log(bitcoin.address.fromBech32(addr));
+    log(script.toString('hex'));
+    log(bitcoin.script.toASM(script));
+    log(bitcoin.script.decompile(script));
+    
+    return;
+
     let hash = bitcoin.crypto.sha256(script)
     let reversedHash = new Buffer.from(hash.reverse())
 
     let rScriptHash = reversedHash.toString('hex');
 
-    console.log("Address: ",  terminalLink(addr, `https://blockchair.com/bitcoin/address/${addr}`));
+    log("Address: ",  terminalLink(addr, `https://blockchair.com/bitcoin/address/${addr}`));
+    log("Redeem Script: ", script.toString('hex'));
+    log(script)
 
-    let history;
     if(options.verbose){
-      console.log("Script: ", script[1]);
+      let arr = [...script];
+      console.log("Script: ", arr[2].toString(16));
       console.log("Script: ", script.toString('hex'));
       console.log("Electrum reversed script_hash: ", rScriptHash);
-      history = await client.blockchain_scripthash_getHistory(rScriptHash);
     }
 
     let balance = await client.blockchain_scripthash_getBalance(rScriptHash)
-    let UTXOs = await client.blockchain_scripthash_listunspent(rScriptHash);
     
+    const BTC_UNIT = 100000000;
     
-    let btcBalance = balance.confirmed/100000000;
+    let btcBalance = balance.confirmed/BTC_UNIT;
     let btcPrice = (await axios.get('https://blockchain.info/q/24hrprice')).data;
     
-    console.log('\nBalance:', chalk.green(btcBalance + `BTC (~ ${(btcBalance*btcPrice).toFixed(2)} USD)`));
+    log('Balance:', chalk.green(btcBalance + `BTC (~ ${(btcBalance*btcPrice).toFixed(2)} USD)`));
     
-    console.log(`UTXOs (${UTXOs.length})`);
-    UTXOs.forEach(utxo => {
-        console.log("Hash: ", terminalLink(utxo.tx_hash, `https://blockchair.com/bitcoin/transaction/${utxo.tx_hash}`));
-        console.log("Value:", chalk.blue(utxo.value / 100000000 + " BTC "));
-    })
+    if(options.verbose){
 
-    if(history){
-      console.log("Transaction History")
+      let UTXOs = await client.blockchain_scripthash_listunspent(rScriptHash);
+      log(`UTXOs (${UTXOs.length})`);
+      UTXOs.forEach(utxo => {
+        log("Hash: ", terminalLink(utxo.tx_hash, `https://blockchair.com/bitcoin/transaction/${utxo.tx_hash}`));
+        log("Value:", chalk.blue(utxo.value / BTC_UNIT + " BTC "));
+      });
+
+      log("Transaction History")
+      let history = await client.blockchain_scripthash_getHistory(rScriptHash);
       history.forEach(h=> {
         log("TX ID:", terminalLink(h.tx_hash, `https://blockchair.com/bitcoin/transaction/${h.tx_hash}`));
       })
