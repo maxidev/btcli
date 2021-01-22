1effbae6e6a5b5ae1c6c432f564ce94d47e0790f
Maxi Dev <maxi.canellas@gmail.com>
2021-01-22 16:36:30 -0300

    [FIX ] parse verbose=false tx info

--- scripts/transaction.js
+++ scripts/transaction.js
@@ -1,8 +1,25 @@
+const chalk = require('chalk');
+const terminalLink = require('terminal-link');
+
 async function transaction(client, options) {
   try {
     const transaction = options.transaction;
     let tx  = await client.blockchain_transaction_get(transaction, true);
-    console.log(tx);
+
+    let { txid, size, vin, vout, blockhash, confirmations, time } = tx;
+    
+    if(options.verbose){
+
+    }else{
+      log("ID: ", chalk.blue(terminalLink(txid, `https://blockchair.com/bitcoin/transaction/${txid}`)));
+      log("Block Hash: ", chalk.green(terminalLink(blockhash, `https://blockchair.com/bitcoin/block/${blockhash}`)));
+      log("Confirmations: ", chalk.green(confirmations));
+      log("Size: ", chalk.green(size + " bytes"))
+      const dateObject = new Date(time*1000)
+      const humanDateFormat = dateObject.toLocaleString() //2019-12-9 10:30:15
+      log("Timestamp: ", chalk.green(time + ` (${humanDateFormat} UTC)`));
+    }
+    
   } catch (err) {
     console.log(err);
   }
