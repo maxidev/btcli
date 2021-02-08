06ae3962a64ecac45eb73ea6105bbeccbe0b029b
Maxi Dev <maxi.canellas@gmail.com>
2021-02-08 15:18:00 -0300

    [FIX] multiple addr, verbose and testnet fixes ready for RC 0.1

--- utils/address.js
+++ utils/address.js
@@ -1,22 +1,37 @@
 const bitcoin = require('bitcoinjs-lib');
+const { isTestnet } = require('./args');
 
 const LEGACY_ADDR = 'Legacy (P2PKH)';
 const BECH32_ADDR = 'SegWit (Bech32)';
 const SCRIPT_HASH_ADDR = 'Nested Segwit / Legacy P2SH';
+const TESTNET_SCRIPT_HASH = "MultiSig TESTNET";
 
 /* 
   Detect if address is valid checksum / format
 */
 function isValid(_address) {
   try {
-    if (_address[0] == 1 || _address[0] == 3) {
-      bitcoin.address.fromBase58Check(_address);
-      return true;
-    } else if (_address[0] == 'b' && _address[1] == 'c' && _address[2] && 1) {
-      bitcoin.address.fromBech32(_address);
-      return true;
-    } else {
-      return false;
+
+    if(isTestnet()){
+      if(_address[0] == 'm' || _address[0] == 'n' || _address[0] == '2'){
+        bitcoin.address.fromBase58Check(_address);
+        return true;
+      }else if(_address[0] == 't' && _address[1] == 'b' && _address[2] == 1){
+        bitcoin.address.fromBech32(_address);
+        return true;
+      }else{
+        return false;
+      }
+    }else{
+      if (_address[0] == 1 || _address[0] == 3) {
+        bitcoin.address.fromBase58Check(_address);
+        return true;
+      } else if (_address[0] == 'b' && _address[1] == 'c' && _address[2] == 1) {
+        bitcoin.address.fromBech32(_address);
+        return true;
+      } else {
+        return false;
+      }
     }
   } catch (error) {
     return false;
@@ -34,12 +49,23 @@ function getType(_address) {
     if (_address[0] == 1) {
       bitcoin.address.fromBase58Check(_address);
       return LEGACY_ADDR;
-    } else if (_address[0] == 'b' && _address[1] == 'c' && _address[2] && 1) {
+    } else if (_address[0] == 'b' && _address[1] == 'c' && _address[2] == 1) {
       bitcoin.address.fromBech32(_address);
       return BECH32_ADDR;
     } else if (_address[0] == 3) {
       bitcoin.address.fromBase58Check(_address);
       return SCRIPT_HASH_ADDR;
+    }else if(_address[0] == 'm' || _address[0] == 'n'){
+      bitcoin.address.fromBase58Check(_address);
+      return LEGACY_ADDR + "[TESTNET]";
+    }else if(_address[0] == 't' && _address[1] == 'b' && _address[2] == 1){
+      bitcoin.address.fromBech32(_address);
+      return BECH32_ADDR + "[TESTNET]";
+    }else if(_address[0] == 2){
+      bitcoin.address.fromBase58Check(_address);
+      return TESTNET_SCRIPT_HASH;
+    }else{
+      return "Unknown Address Type";
     }
   } catch (error) {
     console.log(error);
--- utils/connect.js
+++ utils/connect.js
@@ -1,6 +1,9 @@
 const { getServer, isTestnet } = require('./args');
 const ElectrumClient = require('@codewarriorr/electrum-client-js');
 const servers = require('../servers.json');
+const ora = require('ora');
+const spinner = ora();
+spinner.spinner = 'squareCorners';
 
 async function connect() {
   // Public Electrum server list: https://1209k.com/bitcoin-eye/ele.php?chain=btc
@@ -8,11 +11,12 @@ async function connect() {
   try {
     const { url, port, proto } = isTestnet() ? servers.TESTNET : servers.BAREMETALPITTSBURGH;
     const client = new ElectrumClient(url, port, proto);
-  
+    spinner.start();
     await client.connect();
   
     console.log('------------------------------------------------');
-  
+    spinner.clear();
+    spinner.stop();
     return client;  
   } catch (error) {
     console.log(error);    
